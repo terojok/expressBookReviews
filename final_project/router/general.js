02763c54e829993e055c26c21d7d44a72155629f
terojok <terojok@live.com>
2024-02-01 12:38:14 -0500

    Add task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,13 +17,17 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const book = books[req.params.isbn];
+    if (book) {
+        res.send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404).json( { message: "Not found" } );
+    }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
