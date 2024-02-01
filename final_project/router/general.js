9fe0b1dcd9d72f658bb7e0faaa7e6c47732486bf
terojok <terojok@live.com>
2024-02-01 14:16:33 -0500

    Add task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,32 @@ let users = require("./auth_users.js").users;
 
 const public_users = express.Router();
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+function isNonEmptyString(value) {
+    return (typeof value === 'string') && (value.trim().length > 0);
+}
+
+public_users.post("/register", function (req, res) {
+    const username = req.body.username;
+    if (!isNonEmptyString(username)) {
+        res.status(400).json( { message: "Missing or invalid username" } );
+        return;
+    }
+
+    const password = req.body.password;
+    if (!isNonEmptyString(password)) {
+        res.status(400).json( { message: "Missing or invalid password" } );
+        return;
+    }
+
+    for (const user of users) {
+        if (user.username === username) {
+            res.status(409).json( { message: "User already exists" } );
+            return;    
+        }
+    }
+
+    users.push( { "username": username, "password": password } );
+    res.json( { message: "User successfully registered" } );
 });
 
 // Get the book list available in the shop
@@ -22,7 +45,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
     if (book) {
         res.send(JSON.stringify(book, null, 4));
     } else {
-        return res.status(404).json( { message: "Not found" } );
+        res.status(404).json( { message: "Not found" } );
     }
 });
 
@@ -54,7 +77,7 @@ public_users.get('/review/:isbn', function (req, res) {
     if (book) {
         res.send(JSON.stringify(book.reviews, null, 4));
     } else {
-        return res.status(404).json( { message: "Not found" } );
+        res.status(404).json( { message: "Not found" } );
     }
 });
 
